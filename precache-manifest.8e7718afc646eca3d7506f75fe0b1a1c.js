9ae5891450713f6b87c5dec0f388d2219a7627af
Miguel Angel <angelarkangel91@hotmail.com>
2021-01-24 16:10:03 -0600

    Updates

---
+++ precache-manifest.8e7718afc646eca3d7506f75fe0b1a1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4feff53fb3cf9cdc84832f1d189440c2",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "260faa65b03d8affdc27",
+    "url": "/todoapp/static/css/main.95afebba.chunk.css"
+  },
+  {
+    "revision": "a1e46d20879d7cda8def",
+    "url": "/todoapp/static/js/2.930ed9bd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todoapp/static/js/2.930ed9bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260faa65b03d8affdc27",
+    "url": "/todoapp/static/js/main.802f5088.chunk.js"
+  },
+  {
+    "revision": "ea3b9a0f3a8bd4a0f1d0",
+    "url": "/todoapp/static/js/runtime-main.628c2681.js"
+  }
+]);
